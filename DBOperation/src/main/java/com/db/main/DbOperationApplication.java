8e7eb2bd14595a917e7aac808901ee22c461fd23
dubeypriya <47915517+dubeypriya@users.noreply.github.com>
2020-02-20 16:28:44 +0530

    Update DbOperationApplication.java

--- DBOperation/src/main/java/com/db/main/DbOperationApplication.java
+++ DBOperation/src/main/java/com/db/main/DbOperationApplication.java
@@ -1,9 +1,10 @@
 package com.db.main;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @EnableAutoConfiguration
